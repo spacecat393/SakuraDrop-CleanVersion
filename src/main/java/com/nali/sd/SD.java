8292018cd6f97abf6abbdcf0d143634e4b90b3b4
SpaceCat <jjcakecat@gmail.com>
2024-02-24 18:09:10 +0700

    clean

--- src/main/java/com/nali/sd/SD.java
+++ src/main/java/com/nali/sd/SD.java
@@ -6,8 +6,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME)
 public class SD
@@ -15,7 +13,6 @@ public class SD
     @Instance
     public static SD I;
 
-    public static Logger LOGGER = LogManager.getLogger(Reference.MOD_ID);
     public static Configuration CONFIGURATION;
 
     @EventHandler
